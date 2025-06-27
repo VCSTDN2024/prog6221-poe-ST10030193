e953e5ed94e52c00d7bec7cd3d8a0a63126cce1f
ST10030193 <104854137+ST10030193@users.noreply.github.com>
2025-06-27 16:57:47 +0200

    InitialCommit

---
+++ MiniProjectGUI/MiniProjectGUI/Logic/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjectGUI.Logic
+{
+    internal class UserProfile
+    {
+            public string Interest { get; set; }
+            public string FavouriteTopic { get; set; }
+        
+    }
+}
---
+++ MiniProjectGUI/MiniProjectGUI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MiniProjectGUI.Logic;
+
+namespace MiniProjectGUI
+{
+    public partial class MainWindow : Window
+    {
+
+        private UserQuery chatbot;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            chatbot = new UserQuery("User", "");
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userMessage = UserInputTextBox.Text.Trim();
+
+            if (!string.IsNullOrWhiteSpace(userMessage))
+            {
+                ChatListBox.Items.Add($"You: {userMessage}");
+
+                string botReply = chatbot.GetBotResponse(userMessage); 
+                ChatListBox.Items.Add($"Bot: {botReply}");
+
+                UserInputTextBox.Clear();
+            }
+        }
+    }
+}
+
